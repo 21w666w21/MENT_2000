b24830139dc973d69617de5bfecfc4bc45651f43
21w666w21 <21w666w21@rambler.ru>
2013-10-29 14:41:25 +0600

    Рисование линий вместо крестиков

--- MENT_2000/Form1.cs
+++ MENT_2000/Form1.cs
@@ -11,8 +11,10 @@ namespace MENT_2000
 {
     public partial class Form1 : Form
     {
-        List<Cross> Points = new List<Cross>();
+       // List<Cross> Points = new List<Cross>();
+        List<Line> Points = new List<Line>();
         bool klik = true;
+        Point p1, p2;
 
 
         public Form1()
@@ -30,26 +32,30 @@ namespace MENT_2000
             if (radioButton1.Checked)
             {
                 this.Text = Convert.ToString(e.X) + " " + Convert.ToString(e.Y);
+                //Points.Add(new Cross(e.Location));
+
             }
             else if (radioButton2.Checked)
             {
                 if (klik)
                 {
-                    this.Text = Convert.ToString(e.X) + " " + Convert.ToString(e.Y) + " четный";
+                    this.Text = Convert.ToString(e.X) + " " + Convert.ToString(e.Y) + " первый";
+                    p1 = e.Location;
                 }
                 else
                 {
-                    this.Text = Convert.ToString(e.X) + " " + Convert.ToString(e.Y) + " нечетный";
+                    this.Text = Convert.ToString(e.X) + " " + Convert.ToString(e.Y) + " второй";
+                    p2 = e.Location;
+                    Points.Add(new Line(p1, p2));
                 }
                 klik = !klik;
             }
-            Points.Add(new Cross(e.Location));
             Invalidate();
         }
 
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
-            foreach (Cross count in this.Points)
+            foreach (Line count in this.Points)
             {
                 count.ReDraw(e.Graphics);
             }
@@ -77,6 +83,23 @@ namespace MENT_2000
         {
             g.DrawLine(p, a.X - 2, a.Y - 2, a.X + 2, a.Y + 2);
             g.DrawLine(p, a.X - 2, a.Y + 2, a.X + 2, a.Y - 2);
+        }
+    }
+    public class Line
+    {
+        Point s, f;
+
+        Pen w = new Pen(Color.Black);
+
+        public Line(Point s, Point f)
+        {
+            this.s = s;
+            this.f = f;
+        }
+
+        public void ReDraw(Graphics g)
+        {
+            g.DrawLine(w, s, f);
         }
     }
     
